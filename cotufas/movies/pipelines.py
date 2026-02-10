cb3aea2665cbf16348b76027cd7eac3fa2d2d1e1
jaimelgc <jaimeleongarcia123@gmail.com>
2026-02-10 14:48:51 +0000

    save

---
+++ cotufas/movies/pipelines.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+cinemas = ['yelmo', 'multicines', 'zentralcenter', 'xsur']
+
+INPUT_PATH = 'data_raw/'
+OUTPUT_PATH = 'data/'
+
+
+def normalize_data(path1: str, path2: str):
+    '''Función que normaliza y selecciona los datos
+    a utilizar entre todos los cines y sesiones'''
+    pass
+
+    # df_input = pd.read_json

696b3c2a6c6157743768c5ea0821dbae6d704634
jaimelgc <jaimeleongarcia123@gmail.com>
2026-02-10 14:32:58 +0000

    save

--- cotufas/movies/models.py
+++ cotufas/movies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    synopsis = models.TextField()
+    age = models.IntegerField()
+    showings = {}
+    genres = []
+    directors = []
+    actors = []
